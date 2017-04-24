b1b1191ab1b01856461830c782a577a1c222becc
Chuong Le <chuong@poeticsystems.com>
2017-04-24 01:04:42 -0500

    fix incorrect variable for Drupal url.

--- data/connectors.js
+++ data/connectors.js
@@ -6,14 +6,14 @@ const DRUPAL_URL = env.drupal.url;
 const Camden = {
   getCommunities(args) {
     if (args !== undefined) {
-      return rp(url + 'node/' + args.nid + '?_format=json')
+      return rp(DRUPAL_URL + 'node/' + args.nid + '?_format=json')
         .then((res) => JSON.parse(res))
         .then((res) => {
           return { nid: res.nid[0].value, title: res.title[0].value };
         });
     }
 
-    return rp(url + 'api/communities')
+    return rp(DRUPAL_URL + 'api/communities')
       .then((res) => JSON.parse(res))
       .then((res) => {
         return res;
