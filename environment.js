e2ded82539541bf80a23a6f5b2fa26c9883739b2
Chuong Le <chuong@poeticsystems.com>
2017-04-24 01:05:14 -0500

    fix package.json and envrionment.js

--- environment.js
+++ environment.js
@@ -4,7 +4,7 @@ let env = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   drupal: {
-    url: process.env.DRUPAL_URL,
+    url: process.env.DRUPAL_URL || 'http://dev-d8-headless.pantheonsite.io/',
   }
 }
 
